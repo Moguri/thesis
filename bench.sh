58459011e57a5fe3c67d3bb47f130d42d055cd67
Mitchell Stokes <mogurijin@gmail.com>
2014-01-07 10:42:43 -0800

    Adding the benchmarking script and blend file to this git repo.

---
+++ bench.sh
@@ -0,0 +1,3 @@
+#LD_PRELOAD="/usr/lib/libprofiler.so" CPUPROFILE=bench.prof CPUPROFILE_FREQUENCY=10000 blenderplayer -w 1600 900 -g show_framerate = 1 -g show_profile = 1 ~/Dropbox/blends/anim_stress_xheavy.blend
+
+blenderplayer -w 1600 900 -g show_framerate = 1 -g show_profile = 1 ~/Dropbox/blends/anim_stress_xheavy.blend
