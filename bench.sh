13764b62a5928bae950769aa0d271f301e76bf02
Mitchell Stokes <mogurijin@gmail.com>
2014-01-09 14:13:32 -0800

    Adding a way to gather profile data on 0-n armature scenes

--- bench.sh
+++ bench.sh
@@ -1,3 +1,5 @@
 #LD_PRELOAD="/usr/lib/libprofiler.so" CPUPROFILE=bench.prof CPUPROFILE_FREQUENCY=10000 blenderplayer -w 1600 900 -g show_framerate = 1 -g show_profile = 1 blends/anim_stress_xheavy.blend
 
-blenderplayer -w 1600 900 -g show_framerate = 1 -g show_profile = 1 blends/anim_stress_xheavy.blend
+while [ 1 ]; do
+	blenderplayer -w 1600 900 -g show_framerate = 1 -g show_profile = 1 blends/anim_stress_xheavy.blend && break;
+done
